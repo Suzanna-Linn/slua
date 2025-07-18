8b90674f40028f61c89e45a180281f4ebafaca86
Suzanna-Linn <Suzanna.slife@gmail.com>
2025-07-18 11:03:48 +0200

    Update lua.js

--- docs/prism/lua.js
+++ docs/prism/lua.js
@@ -7,6 +7,7 @@ Prism.languages.lua = {
 	},
 	'number': /\b0x[a-f\d]+(?:\.[a-f\d]*)?(?:p[+-]?\d+)?\b|\b\d+(?:\.\B|(?:\.\d*)?(?:e[+-]?\d+)?\b)|\B\.\d+(?:e[+-]?\d+)?\b/i,
 	'keyword': /\b(?:and|break|do|else|elseif|end|false|for|function|goto|if|in|local|nil|not|or|repeat|return|then|true|until|while)\b/,
+	'builtin': /\b(?:print)\b/,
 	'function': /(?!\d)\w+(?=\s*(?:[({]))/,
 	'operator': [
 		/[-+*%^&|#]|\/\/?|<[<=]?|>[>=]?|[=~]=?/,
