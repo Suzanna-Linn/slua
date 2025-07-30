9cdcceca675865401404cef4a2e12b2bed783ed0
Suzanna-Linn <Suzanna.slife@gmail.com>
2025-07-30 09:00:55 +0200

    Update theme-switch-button.js

--- docs/assets/theme-switch-button.js
+++ docs/assets/theme-switch-button.js
@@ -8,7 +8,7 @@
   if (savedTheme === 'night') {
     dayTheme.disabled = true;
     nightTheme.disabled = false;
-    button.textContent = 'Script theme to light';
+    button.textContent = 'Light script theme';
   }
 
   button.addEventListener('click', function () {
@@ -19,13 +19,13 @@
       dayTheme.disabled = false;
       nightTheme.disabled = true;
       localStorage.setItem('theme', 'day');
-      button.textContent = 'Script theme to dark';
+      button.textContent = 'Dark script theme';
     } else {
       // Switch to night
       dayTheme.disabled = true;
       nightTheme.disabled = false;
       localStorage.setItem('theme', 'night');
-      button.textContent = 'Script theme to light';
+      button.textContent = 'Light script theme';
     }
 
     // Remove focus from button
