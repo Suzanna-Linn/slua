06169b9c518f52d7178bebdc798efebe700a8162
Suzanna-Linn <Suzanna.slife@gmail.com>
2025-08-02 10:51:52 +0200

    Update language-lsl.js

--- docs/assets/language-lsl.js
+++ docs/assets/language-lsl.js
@@ -17,7 +17,7 @@ Prism.languages.lsl = {
         pattern: /\b(?:float|integer|key|list|quaternion|rotation|string|vector)\b/,
         alias: 'keyword'
     },
-    'builtin': /\b(?:at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|timer|touch|touch_end|touch_start|transaction_result)\b/,
+    'builtin': /\b(?:at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|state_entry|state_exit|timer|touch|touch_end|touch_start|transaction_result)\b/,
     'builtin-ll': {
         pattern: /\bll\w+(?=\s*(?:[({]))/,
         alias: 'builtin'
