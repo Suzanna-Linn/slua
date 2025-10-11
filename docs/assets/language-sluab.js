ed885b8dc481e4b0b13db6ae036b007ae606cd61
Suzanna Linn <Suzanna.slife@gmail.com>
2025-10-11 09:24:03 +0200

    Update language-sluab.js

--- docs/assets/language-sluab.js
+++ docs/assets/language-sluab.js
@@ -101,6 +101,10 @@ Prism.languages.sluab = {
         pattern: /\b(?:assert|error|gcinfo|getmetatable|ipairs|newproxy|next|pairs|pcall|print|rawequal|rawget|rawlen|rawset|select|setmetatable|tonumber|tostring|type|typeof|unpack|xpcall|integer|quaternion|rotation|toquaternion|torotation|tovector|uuid|vector)\b(?=\s*(?:[({]))/,
         alias: 'builtin'
     },
+    'builtin-librzries': {
+        pattern: /\b(?:bit32|buffer|coroutine|debug|os|string|table|utf8|vector|ll|llbase64|llcompat|llevents|lljson|lltimers)\b(?=\s*(?:[({]))/,
+        alias: 'builtin'
+    },
     'builtin-metamethods': {
         pattern: /\b__(?:add|call|concat|div|eq|idiv|index|iter|len|le|lt|metatable|mode|mod|mul|newindex|pow|sub|tostring|unm|tojson)\b/,
         alias: 'builtin'
