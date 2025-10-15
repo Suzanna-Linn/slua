b1cf29e1dd1f830e36bc90a5c65751d238bc91a0
Suzanna Linn <Suzanna.slife@gmail.com>
2025-10-15 10:04:42 +0200

    Update language-sluab.js

--- docs/assets/language-sluab.js
+++ docs/assets/language-sluab.js
@@ -10,7 +10,7 @@ Prism.languages.sluab = {
         }
     },
     'builtin-event-string': {
-        pattern: /(llevents\.(?:on|off|once|listeners)\()\s*(['"])(?:at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|touch|touch_end|touch_start|transaction_result)\2/,
+        pattern: /(LLEvents:(?:on|off|once|listeners)\()\s*(['"])(?:at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|touch|touch_end|touch_start|transaction_result)\2/,
         lookbehind: true,
         alias: 'builtin'
     },
@@ -85,11 +85,15 @@ Prism.languages.sluab = {
         alias: 'builtin'
     },
     'builtin-llevents': {
-        pattern: /llevents\.(?:on|once|off|eventNames|listeners|at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|touch|touch_end|touch_start|transaction_result)\b/,
+        pattern: /llevents:(?:on|once|off|eventNames|listeners)\b/,
+        alias: 'builtin'
+    },
+    'builtin-llevents-alternative': {
+        pattern: /llevents\.(?:at_rot_target|at_target|attach|changed|collision|collision_end|collision_start|control|dataserver|email|experience_permissions|experience_permissions_denied|final_damage|game_control|http_request|http_response|land_collision|land_collision_end|land_collision_start|link_message|linkset_data|listen|money|moving_end|moving_start|no_sensor|not_at_rot_target|not_at_target|object_rez|on_damage|on_death|on_rez|path_update|remote_data|run_time_permissions|sensor|touch|touch_end|touch_start|transaction_result)\b/,
         alias: 'builtin'
     },
     'builtin-lltimers': {
-        pattern: /lltimers\.(?:on|once|off)\b/,
+        pattern: /lltimers:(?:on|once|off)\b/,
         alias: 'builtin'
     },
     'builtin-get': {
