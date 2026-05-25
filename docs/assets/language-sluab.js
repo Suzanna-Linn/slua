e9d0dfbcabb17b8d5462aba0115c94554b6d41f5
Suzanna Linn <Suzanna.slife@gmail.com>
2026-05-25 11:43:26 +0200

    Fix syntax error in language-sluab.js

--- docs/assets/language-sluab.js
+++ docs/assets/language-sluab.js
@@ -35,7 +35,7 @@ Prism.languages.sluab = {
     'public': {
         pattern: /\bpublic(?=\s+(?:function\b|[a-zA-Z_]\w*))/,
         alias: 'keyword'
-    }
+    },
     'builtin-math': {
         pattern: /\bmath\.(?:abs|acos|asin|atan|atan2|ceil|clamp|cos|cosh|deg|exp|floor|fmod|frexp|isfinite|isinf|isnan|ldexp|log|log10|max|min|modf|noise|pow|rad|random|randomseed|round|sign|sin|sinh|sqrt|tan|tanh)\b(?=\s*(?:[({]))/,
         alias: 'builtin'
